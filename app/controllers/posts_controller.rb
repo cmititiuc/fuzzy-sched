b9cea14d20652a0233dedbb40fe27fe8bd2ddc83
m <m@ophect.me>
2016-08-16 15:05:09 -0400

    adds the footer to the posts index view; refactors a bit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  layout 'welcome'
   skip_before_filter :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def show_nav_link(models)
+    style = 'font-weight: bold; font-size: medium; text-decoration: none;'
+    if controller_name == models
+      link_to models.titleize, send("#{models}_path"), :style => style
+    else
+      link_to models.titleize, send("#{models}_path")
+    end
+  end
 end
